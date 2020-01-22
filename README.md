055111032feeb801a0036f102837333d40907358
Fotis Evangelou <fevangelou@users.noreply.github.com>
2020-01-22 14:09:00 +0200

    Update readme and credits

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
 # KDK Change Locale
 
 ## Description
+
 Change a WordPress site's locale based on active category.
 
 This plugin assists when creating multi-lingual sites where each language is essentially a top-level category. By using category slugs like "en" or "es", this plugin can detect from the URL path the locale to set the site to.
 
-E.g. if the category slug is "en", the locale will be set to "en_US" for US English. If it's "es", the locale will be set to "es_ES" for Spanish and so on.
+E.g. if the category slug is "en", the locale will be set to `en_US` for US English. If it's "es", the locale will be set to `es_ES` for Spanish and so on.
 
-## Installation
-Download the repo and create a .zip archive. Install it like any other WordPress plugin.
 
 ## Requirements
+
 You need to have the desired languages installed in your WordPress site so the site's frontend (interface) properly switches to the correct language.
 
+
 ## Modify
+
 Define extra languages in the '$languages' array.
 
+
 ## To Do
+
 Add option to dynamically set the languages based on installed language packs in WordPress or add a WP backend UI to manually set the languages.
+
+
+## Installation
+
+Download the repo from https://github.com/kodeka/kdk_change_locale/archive/master.zip and extract the zip file. Rename the created folder to "kdk_change_locale". Zip that folder. Install the zip file like any other WordPress plugin through the plugin manager.
+
+
+## License & Credits
+
+Licensed under the GNU/GPL license (https://www.gnu.org/copyleft/gpl.html).
+
+Copyright (c) 2018 - 2020 Kodeka OÜ. All rights reserved.
--- kdk-change-locale.php
+++ kdk-change-locale.php
@@ -1,56 +1,44 @@
 <?php
 /*
     Plugin Name: KDK Change Locale
-    Plugin URI:  https://github.com/joomlaworks/kdk-change-locale
+    Plugin URI:  https://github.com/kodeka/kdk_change_locale
     Description: Change the site's locale based on active category
     Version:     1.1
-    Author:      JoomlaWorks
-    Author URI:  https://www.joomlaworks.net
-    License:     GNU/GPL v2
+    Author:      Kodeka
+    Author URI:  https://kodeka.io
+    License:     GNU/GPL license: https://www.gnu.org/copyleft/gpl.html
 */
 defined('ABSPATH') or die('Move along');
 
-/**
- * Change the site's locale based on active category
- *
- * @param   $locale
- * @return  the language locale for the frontend, the language needs to be already installed
- * @uses    $_SERVER[HTTP_HOST] and $_SERVER[REQUEST_URI] in order to get the URL
- * @version 1.1
- * @link    https://github.com/joomlaworks/kdk-change-locale
- */
-
 function kdk_set_locale($locale)
 {
-    
     // Don't execute in the backend
     if (is_admin()) {
         return;
     }
-    
+
     /*
     $protocol = (stripos($_SERVER['SERVER_PROTOCOL'],'https') === true) ? 'https://' : 'http://';
     $url_path = $protocol.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
     */
     $url_path = $_SERVER['REQUEST_URI'];
-    
+
     $languages = array(
         'en' => 'en_US', // US English
         'de' => 'de_DE', // German
         'fr' => 'fr_FR', // French
         'es' => 'es_ES', // Spanish
         'ru' => 'ru_RU', // Russian
-        'el' => 'el_GR'  // Greek
     );
-    
+
     $new_locale = '';
-    
+
     foreach ($languages as $cat_slug => $lcl) {
         if (substr($url_path, -3) == '/'.$cat_slug || stripos($url_path, '/'.$cat_slug.'/') !== false || (isset($_GET["lang"]) && $_GET["lang"] === $cat_slug)) {
             $new_locale = $lcl;
         }
     }
-    
+
     if ($new_locale) {
         return $new_locale;
     } else {
